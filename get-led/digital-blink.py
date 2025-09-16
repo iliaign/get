91440370cfecb1cc8cb81937f3bd8c19adcf77d2
Ignatov Ilya <gerbrf12@gamil.com>
2025-09-16 15:04:50 +0300

    Сделал задание с миганием

--- get-led/digital-blink.py
+++ get-led/digital-blink.py
@@ -6,9 +6,9 @@ GPIO.setmode(GPIO.BCM)
 led = 26
 GPIO.setup(led, GPIO.OUT)
 state = 0
-period_popolam = 1.0
+period = 1.0
 
 while True:
     GPIO.output(led, state)
     state = not(state)
-    time.sleep(period_popolam)
+    time.sleep(period/2)
